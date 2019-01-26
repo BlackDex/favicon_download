df6badb21d9c3fc3893c33539df3e9e8edd13481
BlackDex <black.dex@gmail.com>
2019-01-26 18:16:46 +0100

    Some cleaning, layout and comment changes.
    - Renamed some variables
    - Added and changed some comments
    - Restructured some logic

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::env;
 extern crate reqwest;
 use reqwest::Client;
 use reqwest::header::{HeaderMap, HeaderValue, USER_AGENT, ACCEPT_LANGUAGE, CACHE_CONTROL, PRAGMA, ACCEPT};
+use std::time::Duration;
 
 extern crate regex;
 use regex::Regex;
@@ -13,9 +14,6 @@ extern crate soup;
 use soup::prelude::*;
 
 use std::vec::Vec;
-
-use std::time::Duration;
-
 #[derive(Debug)]
 struct IconList {
 	priority: u8,
@@ -25,12 +23,24 @@ struct IconList {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let rawdomain = &args[1];
-    let url = get_icon_url_extra(&rawdomain);
+    let url = get_icon_url(&rawdomain).unwrap();
 
-    println!("\n----------------------\nURL:{}\nRS: {:#?}\nBitwarden: https://icons.bitwarden.com/{}/icon.png\n\n---------------\n", rawdomain, url, rawdomain);
+    println!("\nURL: {:#?}\nRS: {:#?}\n", rawdomain, url);
 }
 
-fn get_icon_url_extra(rawdomain: &str) -> Result<(String), Box<Error>> {
+/// Returns a Result with a String which holds the preferend favicon location.
+/// There will always be a result with a string which will contain https://example.com/favicon.ico
+/// This does not mean that that location does exists, but it is the default location.
+///
+/// # Argument
+/// * `domain` - A string which holds the domain with extension.
+///
+/// # Example
+/// ```
+/// favicon_location1 = get_icon_url("github.com");
+/// favicon_location2 = get_icon_url("gitlab.com");
+/// ```
+fn get_icon_url(domain: &str) -> Result<(String), Box<Error>> {
     // Set some default headers for the request.
     // Use a browser like user-agent to make sure most websites will return there correct website.
     let mut headers = HeaderMap::new();
@@ -46,26 +56,22 @@ fn get_icon_url_extra(rawdomain: &str) -> Result<(String), Box<Error>> {
         .default_headers(headers)
         .build()?;
 
-
-    let ssldomain = format!("https://{}", rawdomain);
-    let httpdomain = format!("http://{}", rawdomain);
-    // A Default SSL URL if for some reason we can not get the websites main page.
-    let mut url = ssldomain.to_string();
-
-    let mut body = "".to_string();
-    let resp = client.get(&ssldomain).send().or_else(|_| client.get(&httpdomain).send());
-    if let Ok(mut content) = resp {
-        body = content.text().unwrap();
-        // Extract the URL from te respose incase redirects occured (like @ gitlab.com)
-        url = format!("{}://{}", content.url().scheme(), content.url().host().unwrap());
-    }
+    // Default URL with secure and insecure schemes
+    let ssldomain = format!("https://{}", domain);
+    let httpdomain = format!("http://{}", domain);
 
     // Create the iconlist
     let mut iconlist: Vec<IconList> = Vec::new();
-    // Add the default favicon.ico to the list
-    iconlist.push(IconList { priority: 35, href: format!("{}{}", url, "/favicon.ico") });
 
-    if ! body.is_empty() {
+    let resp = client.get(&ssldomain).send().or_else(|_| client.get(&httpdomain).send());
+    if let Ok(mut content) = resp {
+        let body = content.text().unwrap();
+        // Extract the URL from te respose incase redirects occured (like @ gitlab.com)
+        let url = format!("{}://{}", content.url().scheme(), content.url().host().unwrap());
+
+        // Add the default favicon.ico to the list with the domain the content responded from.
+        iconlist.push(IconList { priority: 35, href: format!("{}{}", url, "/favicon.ico") });
+
         let soup = Soup::new(&body);
         // Search for and filter
         let favicons = soup
@@ -82,17 +88,16 @@ fn get_icon_url_extra(rawdomain: &str) -> Result<(String), Box<Error>> {
 
             iconlist.push(IconList { priority: favicon_priority, href: favicon_href})
         }
+    } else {
+        // Add the default favicon.ico to the list with just the given domain
+        iconlist.push(IconList { priority: 35, href: format!("{}{}", ssldomain, "/favicon.ico") });
     }
 
+    // Sort the iconlist by priority
     iconlist.sort_by_key(|x| x.priority);
-    //println!("{:#?}", iconlist);
 
-    let mut iconurl = "".to_string();
-    if let Some(icon) = iconlist.first() {
-        iconurl = format!("{}", icon.href);
-    }
-
-    Ok(iconurl)
+    // There always is an icon in the list, so no need to check if it exists, and just return the first one
+    Ok(format!("{}", &iconlist[0].href))
 }
 
 /// Returns a Integer with the priority of the type of the icon which to prefer.
